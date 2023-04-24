78ed574b1fb05ddad765d717b2b30de2165dc961
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 21:30:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+//your Jlet btn= document.getElement.id;
+//your JS code here. If required.
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  document.getElementById("para").innerText = "Entered Metaverse";
+  document.getElementById("para").style.fontSize = "3em";
+	 document.getElementById("para").style.fontWeight = "bolder";
+});
